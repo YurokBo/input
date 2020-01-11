fa053f0c17763700f7eaf005b33ed69d0f206f23
YurokBo <vertel3012@mail.ru>
2020-01-11 18:06:12 +0300

    done

---
+++ app/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('keydown', e => {
+
+    if (e.keyCode === 13) {
+        const inputText = document.querySelector('.input'),
+            div = document.createElement('div');
+        /*div.style.font = '25px';
+        div.style.fontWeight = 'bold';
+        div.style.fontFamily = 'sans-serif';
+        div.style.margin = '5px';*/
+        div.innerHTML = inputText.value;
+        inputText.value = '';
+
+        document.body.append(div);
+
+    }
+})
+
+
