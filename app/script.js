79183bb310c597c4f8b32442262804e9dda5313b
YurokBo <vertel3012@mail.ru>
2020-01-11 18:18:43 +0300

    padding input changed

--- app/script.js
+++ app/script.js
@@ -3,10 +3,10 @@ document.addEventListener('keydown', e => {
     if (e.keyCode === 13) {
         const inputText = document.querySelector('.input'),
             div = document.createElement('div');
-        /*div.style.font = '25px';
+        div.style.font = '25px';
         div.style.fontWeight = 'bold';
         div.style.fontFamily = 'sans-serif';
-        div.style.margin = '5px';*/
+        div.style.margin = '5px';
         div.innerHTML = inputText.value;
         inputText.value = '';
 
